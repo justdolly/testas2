9dc919fc3e3c74f03d76da9898afc1b5fc4957e5
justdolly <156876333+justdolly@users.noreply.github.com>
2025-04-16 19:17:26 -0700

    Add files via upload

---
+++ data/discordStatus.js
@@ -0,0 +1,21 @@
+// Discord Status data - editable without build process
+const discordStatus = {
+  discord_user: {
+    id: "123456789012345678",
+    username: "Justdolly_",
+    avatar: "abcdef0123456789abcdef",
+    discriminator: "0",
+    global_name: "Justdolly_"
+  },
+  discord_status: "online",
+  activities: [
+    {
+      name: "Visual Studio Code",
+      type: 0,
+      state: "Editing portfolio",
+      details: "Working on website"
+    }
+  ],
+  listening_to_spotify: false,
+  avatar_url: "https://cdn.discordapp.com/avatars/123456789012345678/abcdef0123456789abcdef.png"
+};
